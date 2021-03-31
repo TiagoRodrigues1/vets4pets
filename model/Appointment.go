b2ea4c6a2328bd0ab9c5cb06a37ca66e28f65575
Vasco <38292@ufp.edu.pt>
2021-03-31 16:22:29 +0100

    appointment novo

--- model/Appointment.go
+++ model/Appointment.go
@@ -1,15 +1,15 @@
 package model
 
-import "github.com/jinzhu/gorm"
+import (
+	"time"
+
+	"github.com/jinzhu/gorm"
+)
 
 type Appointment struct {
 	gorm.Model `swaggerignore:"true"`
-	ShowedUp   bool `json:"showedUp"`
-	AnimalID   uint `gorm:"TYPE:integer REFERENCES animals"`
-	Day        int  `json:"day"`
-	Month      int  `json:"month"`
-	Year       int  `json:"year"`
-	Hour       int  `json:"hour"`
-	Minutes    int  `json:"minutes"`
-	VetID      uint `gorm:"TYPE:integer REFERENCES users"`
+	ShowedUp   bool      `json:"showedUp"`
+	AnimalID   uint      `gorm:"TYPE:integer REFERENCES animals"`
+	Date       time.Time `json:"date"`
+	VetID      uint      `gorm:"TYPE:integer REFERENCES users"`
 }
