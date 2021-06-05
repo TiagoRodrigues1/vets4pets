94ee5958732fd567ea2ff042f821bb2073ff9034
Miguel <miguel.costa30303@gmail.com>
2021-06-05 04:19:46 +0100

    More validations and bug fixes <3

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'myapp.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 const SERVER_IP = 'http://52.47.179.213:8081/api/v1';
 final storage = FlutterSecureStorage();
-
-
 
 main() async {
   WidgetsFlutterBinding.ensureInitialized();
